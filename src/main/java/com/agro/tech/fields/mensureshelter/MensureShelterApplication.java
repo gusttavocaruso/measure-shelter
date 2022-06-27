5b7fb308de4b6ec039d6d5b924960be4eea6eee9
gustacaru <gustavoalmeida26@hotmail.com>
2022-06-27 16:21:53 -0300

    Open

---
+++ src/main/java/com/agro/tech/fields/mensureshelter/MensureShelterApplication.java
@@ -0,0 +1,13 @@
+package com.agro.tech.fields.mensureshelter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MensureShelterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MensureShelterApplication.class, args);
+	}
+
+}
